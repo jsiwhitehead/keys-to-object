a2bbe21dd60e8ec3875e0fd0c19e0621251ceeb7
Jon Whitehead <jsiwhitehead@gmail.com>
2017-11-16 22:11:18 +0000

    Accepts array keys for mapping nested fields

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
+export interface Nested<T> {
+  [key: string]: Nested<T> | T;
+}
+
 export default function keysToObject<T, U = any>(
   keys: U[],
-  valueMap: T | ((k: U, i: number) => T | undefined),
+  valueMap: T | ((k: U, i: number) => T),
   keyMap?: (k: U, i: number) => string,
+  initial?: { [key: string]: T },
+): { [key: string]: T };
+export default function keysToObject<T, U = any>(
+  keys: U[],
+  valueMap: T | ((k: U, i: number) => T),
+  keyMap?: (k: U, i: number) => string | string[],
+  initial?: Nested<T>,
+): Nested<T>;
+export default function keysToObject<T, U = any>(
+  keys: U[],
+  valueMap: T | ((k: U, i: number) => T),
+  keyMap?: (k: U, i: number) => string | string[],
+  initial = {},
 ) {
   const valueFunc = typeof valueMap === 'function';
-  return keys.reduce<{ [key: string]: T }>((res, k, i) => {
+  return keys.reduce<Nested<T>>((res, key, i) => {
     const newValue = valueFunc
-      ? (valueMap as ((k: U, i: number) => T | undefined))(k, i)
+      ? (valueMap as ((k: U, i: number) => T))(key, i)
       : (valueMap as T);
-    return newValue === undefined
-      ? res
-      : { ...res, [keyMap ? keyMap(k, i) : `${k}`]: newValue };
-  }, {});
+    const newKey = keyMap
+      ? keyMap(key, i)
+      : Array.isArray(key) ? key.map(k => `${k}`) : `${key}`;
+    if (typeof newKey === 'string') return { ...res, [newKey]: newValue };
+    newKey.reduce<Nested<T> | T>((r, k, i) => {
+      if (i === newKey.length - 1) r[k] = newValue;
+      else r[k] = r[k] || {};
+      return r[k];
+    }, res);
+    return res;
+  }, initial);
 }
